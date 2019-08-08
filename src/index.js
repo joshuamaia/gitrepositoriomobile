92c0f8ee1f3420a2d60b1f7dd2cd9d3b3e837d3f
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2019-08-08 10:36:00 -0300

    Configurando Reactotron

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import './config/ReactotronConfig';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,6 +15,8 @@ const styles = StyleSheet.create({
     margin: 10,
   },
 });
+
+console.tron.log('Hello World Joshua Maia');
 
 export default function App() {
   return (
